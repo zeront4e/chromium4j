4f59b7c6571792c125084228e1bb917d5138cbcc
zeront4e <zeront4e@dgs-development.eu>
2025-07-11 21:37:28 +0200

    fix: remove public modifier from WindowManagerWindow

--- src/main/java/io/github/zeront4e/c4j/WindowManagerWindow.java
+++ src/main/java/io/github/zeront4e/c4j/WindowManagerWindow.java
@@ -15,6 +15,6 @@ limitations under the License.
 
 package io.github.zeront4e.c4j;
 
-public record WindowManagerWindow(long id, String title) {
+record WindowManagerWindow(long id, String title) {
     //Ignore...
 }
